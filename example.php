a8754c2d3bde47725e7bd79182f5a92c7531912d
Brent Baisley <brenttech@gmail.com>
2014-02-17 21:04:13 -0500

    lightboxes support, images class with search

--- example.php
+++ example.php
@@ -2,11 +2,49 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+/*
+set_error_handler( function($e_num, $e_msg, $e_file, $e_line, $e_context) {
+    echo 'ERROR '.$e_num;
+    echo ' MSG - '.$e_msg;
+    echo " FILE: ".$e_file;
+    echo ' LINE: '.$e_line;
+    echo " CONTEXT: ".print_r($e_context, true);
+    echo "\n\n\n";
+});
+
+set_exception_handler( function($e) {
+    echo 'EXCEPTION ';
+    print_r($e);
+});
+
+*/
+
 $presto = new Presto\Presto();
 $response = new Shutterstock\Response();
 
-$api = new Shutterstock\Api('hackbattle2013','9ed3c46d1b86096a09c33b8b3ff30c2233655178', $presto, $response);
+//$api = new Shutterstock\Api('hackbattle2013','9ed3c46d1b86096a09c33b8b3ff30c2233655178', $presto, $response);
+$api = new Shutterstock\Api('bigstock','f91479918b999e9be6eaa3d437e2e5496fa09a39', $presto, $response);
 
-print_r($api->test());
+//echo $api->authUser('bbaisley','br8ktime');
+$api->setUser('bbaisley', 'dd6864c9d555baa5dcce25540f1905009fedb8d2');
+
+//print_r($api->lightboxes());
+//print_r($api->userInfo());
+//print_r($api->categories());
+
+
+$lightbox_api = new Shutterstock\Lightbox($api, 24490019);
+$lightbox_test = new Shutterstock\Lightbox($api, 19968949);
+//print_r($lightbox_api->publicUrl());
+//print_r($lightbox_api->contents());
+//print_r($lightbox_api->contentsExtended());
+//print_r($api->createLightbox('ApiClient '.date('Hms')));
+//IMAGE IDs: 126709091, 127334102, 126312287, 126312281, 126312260, 126219941, 126053813
+//print_r($lightbox_test->add(array(126709091, 127334102) ));
 
 //print_r($api->similar(15484942, array('sort_method'=>'relevance') ));
+
+$images = new Shutterstock\Images($api);
+//print_r($images->get(126709091));
+//print_r($images->suggestWords(array(126709091, 127334102, 126312287, 126312281)));
+print_r($images->search('dog'));
--- src/Shutterstock/api-client.php
+++ src/Shutterstock/api-client.php
@@ -11,18 +11,20 @@ class Api {
 	
 	public $lang_code = 'en';
 	
-	protected $rest_client = null;
+	public $rest_client = null;
 
 	protected $api_username = null;
 	
 	protected $api_key = null;
 	
-	protected $username = null;
-	protected $user_auth_token = null;
-	
-	protected $search_defaults = array();
-	
+	public $username = null;
+	public $auth_token = null;
+		
 	public $response = null;
+	
+	private $cache = array();
+	
+	private static $categories = null;
 	
 	public function __construct($api_username, $api_key, $rest_client, $response) {
 		$this->api_username = $api_username;
@@ -30,6 +32,11 @@ class Api {
 		$this->rest_client = $rest_client;
 		$this->rest_client->setAuth($api_username, $api_key);
 		$this->response = $response;
+	}
+	
+	public function clearCache() {
+    	$this->cache = array();
+    	self::$categories = null;
 	}
 	
 	public function setLanguage($lang_code) {
@@ -40,60 +47,77 @@ class Api {
 		return $this->protocol.$this->base_url.$path;
 	}
 	
-	public function setUser($username, $password) {
-		$auth_token = $this->getUserAuthToken($username, $password);
+	public function authUser($username, $password) {
+		$this->clearCache();
+		$auth_token = $this->getAuthToken($username, $password);
 		if ( is_null($auth_token) ) {
 			return $auth_token;
 		} else {
 			$this->username = $username;
-			$this->setUserAuthToken($auth_token);
+			$this->setAuthToken($auth_token);
 			return $auth_token;
 		}
 	}
+	public function setUser($username, $auth_token) {
+		$this->username = $username;
+		$this->setAuthToken($auth_token);
+		$this->clearCache();
+	}
 	
-	public function getUser() {
+	public function getAuthToken($username, $password) {
+		$request_url = $this->buildUrl('/auth/customer.json');
+		$request_params = array('username'=>$username, 'password'=>$password);
+		$response = $this->rest_client->post($request_url, $request_params);
+		$auth_token = null;
+		$response = $this->response->process($response);
+		$auth_token = $response->data['auth_token'];
+		return $auth_token;
+	}
+	
+	public function setAuthToken($auth_token) {
+		$this->auth_token = $auth_token;
+	}
+		
+	public function userInfo() {
 		if ( $this->checkUserConfig() ) {
-			$request_url = $this->buildUrl('/customer/'.$this->username.'.json');
-			$response = $this->rest_client->get($request_url, array('auth_token'=>$this->user_auth_token));
-			$this->response->process($response);
+			$request_url = $this->buildUrl('/customers/'.$this->username.'.json');
+			$response = $this->rest_client->get($request_url, array('auth_token'=>$this->auth_token));
+			try {
+			    $response = $this->response->process($response);
+    			
+			} catch(Exception $e) {
+    			$response = null;
+    			echo $e->message;
+			}
+			return $response;
 		} else {
 			return false;
 		}
 	}
 	
-	public function setUserAuthToken($auth_token) {
-		$this->user_auth_token = $auth_token;
-	}
-	
 	public function checkUserConfig() {
-		if ( is_null($this->username) || is_null($this->user_auth_token) ) {
-			trigger_error('No user or user auth token set.', E_USER_WARNING);
+		if ( is_null($this->username) || is_null($this->auth_token) ) {
+			trigger_error('No user and/or no auth token set.', E_USER_WARNING);
 			return false;
 		}
+		return true;
 	}
+
+    public function get($url_path, $params=null) {
+		$request_url = $this->buildUrl($url_path);
+		$response = $this->rest_client->get($request_url, $params);
+		$response = $this->response->process($response);
+		return $response;
+    }
 	
-	public function getUserAuthToken($username, $password) {
-		$request_url = $this->buildUrl('/auth/customer.json');
-		$request_params = array('username'=>$username, 'password'=>$password);
-		$response = $this->rest_client->post($request_url, $request_params);
-		$auth_token = null;
-		$this->response->process($response);
-		$auth_token = $response->data['auth_token'];
-		return $auth_token;
-	}
-		
-	
-	public function getResources() {
+	public function resources() {
 		$request_url = $this->buildUrl('/resources.json');
 		$response = $this->rest_client->get($request_url);
-		$this->response->process($response);
-		print_r($response);
+		return $this->response->process($response);;
 	}
 	
 	public function test() {
-		$request_url = $this->buildUrl('/test/echo.json?test=connect');
-		$response = $this->rest_client->get($request_url);
-		$this->response->process($response);
+		$response = $this->get('/test/echo.json?test=connect');
 		if ( $response->data['test']=='connect' ) {
 			return true;
 		} else {
@@ -102,17 +126,39 @@ class Api {
 	}
 
 	public function categories() {
-		$request_url = $this->buildUrl('/categories.json');
-		$response = $this->rest_client->get($request_url);
-		$this->response->process($response);
-		return $response->data;
+	    if ( is_null(self::$categories) ) {
+            $response = $this->get('/categories.json');
+            if ( $response->meta['is_success'] && $response->meta['http_code']==200 ) {
+                // Cache category list
+                self::$categories = $response;
+            } else {
+                return $response;
+            }
+	    }
+	    return self::$categories;
 	}
 	
 	public function subscriptions() {
-    	
+	    return $this->get('/customers/'.$this->username.'/subscriptions.json', array('auth_token'=>$this->auth_token));
 	}
 	
-	public function lightboxes() {
-    	
+	public function lightboxes($extended=false) {
+	    $params = array('auth_token'=>$this->auth_token);
+	    if ($extended) {
+    	    $url_path = '/extended.json';
+	    } else {
+    	    $url_path = '.json';
+    	    $params['exclude_images'] = 1;
+    	    $params['hero_image_only'] = 1;
+	    }
+    	return $this->get('/customers/'.$this->username.'/lightboxes'.$url_path, $params);
+	}
+	
+	public function createLightbox($name) {
+	    $url = $this->buildUrl('/customers/'.$this->username.'/lightboxes.json');
+    	$response = $this->rest_client->post($url, array('auth_token'=>$this->auth_token, 'lightbox_name'=>$name));
+        $response = $this->response->process($response);
+        return $response;
 	}
 }
+
